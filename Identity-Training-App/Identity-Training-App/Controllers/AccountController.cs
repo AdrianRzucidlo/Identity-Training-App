2314f5854d5fe5baef8bead070cd98199d9519c9
AdrianRzucidlo <adrianrzucidlo2001@gmail.com>
2022-04-26 00:10:10 +0200

    Login-and-register-VM

---
+++ Identity-Training-App/Identity-Training-App/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Identity_Training_App.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
