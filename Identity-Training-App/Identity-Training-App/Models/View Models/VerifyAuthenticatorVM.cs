258d660595e4487395c74a44af7b8dab9fde81fb
AdrianRzucidlo <adrianrzucidlo2001@gmail.com>
2022-05-10 15:31:39 +0200

    VerifyAuthenticatorVM

---
+++ Identity-Training-App/Identity-Training-App/Models/View Models/VerifyAuthenticatorVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity_Training_App.Models.View_Models
+{
+    public class VerifyAuthenticatorVM
+    {
+        [Required]
+        public  string Code { get; set; }
+
+        public string ReturnUrl { get; set; }
+        [Display(Name ="Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
