aad2b663609ffb28d118135dd06fa5f2836c4989
AdrianRzucidlo <adrianrzucidlo2001@gmail.com>
2022-05-18 23:34:05 +0200

    roles-controller-index-action

---
+++ Identity-Training-App/Identity-Training-App/Controllers/RolesController.cs
@@ -0,0 +1,26 @@
+﻿using Identity_Training_App.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity_Training_App.Controllers
+{
+
+    public class RolesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _usermanager;
+        private readonly RoleManager<IdentityUser> _rolemanager;
+
+        public RolesController(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityUser> rolemanager)
+        {
+            _db = db;
+            _usermanager = userManager;
+            _rolemanager = rolemanager;
+        }
+        public IActionResult Index()
+        {
+            var roles = _db.Roles.ToList();
+            return View(roles);
+        }
+    }
+}
