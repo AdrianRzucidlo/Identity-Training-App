86ed4fd2729dbbca34f33958a6b046854f927c19
AdrianRzucidlo <adrianrzucidlo2001@gmail.com>
2022-05-21 19:35:18 +0200

    claims-models

---
+++ Identity-Training-App/Identity-Training-App/Models/View Models/UserClaimsVM.cs
@@ -0,0 +1,20 @@
+﻿namespace Identity_Training_App.Models.View_Models
+{
+    public class UserClaimsVM
+    {
+        public string UserId { get; set; }
+        public List<UserClaim> userClaims { get; set; }
+
+        public UserClaimsVM()
+        {
+            userClaims = new List<UserClaim>();
+        }
+    }
+
+    public class UserClaim
+    {
+        public string ClaimType { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
