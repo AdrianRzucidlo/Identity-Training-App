8ede6a16a33ed8b2e83e0d2d468c3153c8f0588c
AdrianRzucidlo <adrianrzucidlo2001@gmail.com>
2022-05-24 23:55:46 +0200

    authorization-part1

---
+++ Identity-Training-App/Identity-Training-App/Controllers/AccessCheckerController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity_Training_App.Controllers
+{
+    [Authorize]
+    public class AccessCheckerController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult AllAccess()
+        {
+            return View();
+        }
+
+        public IActionResult AuthorizedAccess()
+        {
+            return View();
+        }
+        [Authorize(Roles ="User")]
+        public IActionResult UserAccess()
+        {
+            return View();
+        }
+        public IActionResult AdminAccess()
+        {
+            return View();
+        }
+
+        public IActionResult AdminCreateAccess()
+        {
+            return View();
+        }
+
+        public IActionResult AdminCreateEditDeleteAccess()
+        {
+            return View();
+        }
+
+        public IActionResult AdminCreateEditDeleteSuperUserAccess()
+        {
+            return View();
+        }
+    }
+}
