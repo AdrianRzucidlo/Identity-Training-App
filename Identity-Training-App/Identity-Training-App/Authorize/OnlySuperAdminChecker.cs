15ac7cce7a59fbac5c5271c1637f9a3aa2bf9005
AdrianRzucidlo <adrianrzucidlo2001@gmail.com>
2022-05-29 23:53:52 +0200

    onlysuperadminchecker-class

---
+++ Identity-Training-App/Identity-Training-App/Authorize/OnlySuperAdminChecker.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Identity_Training_App.Authorize
+{
+    public class OnlySuperAdminChecker : AuthorizationHandler<OnlySuperAdminChecker>
+    {
+    }
+}
