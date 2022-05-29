65234199f6fc9dd1453fbb78c05f379bec33e1b7
AdrianRzucidlo <adrianrzucidlo2001@gmail.com>
2022-05-30 01:13:03 +0200

    onlysuperadminchecker

--- Identity-Training-App/Identity-Training-App/Authorize/OnlySuperAdminChecker.cs
+++ Identity-Training-App/Identity-Training-App/Authorize/OnlySuperAdminChecker.cs
@@ -2,7 +2,16 @@
 
 namespace Identity_Training_App.Authorize
 {
-    public class OnlySuperAdminChecker : AuthorizationHandler<OnlySuperAdminChecker>
+    public class OnlySuperAdminChecker : AuthorizationHandler<OnlySuperAdminChecker>, IAuthorizationRequirement
     {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, OnlySuperAdminChecker requirement)
+        {
+            if(context.User.IsInRole("SuperAdmin"))
+            {
+                context.Succeed(requirement);
+                return Task.CompletedTask;
+            }
+            return Task.CompletedTask;
+        }
     }
 }
